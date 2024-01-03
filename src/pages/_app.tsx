5e4d8fc809db1bec0b38cb32917c75d66a031430
Chinecherem Harry <chinecherempharry@gmail.com>
2024-01-03 10:06:40 +0100

    done with the admin section of the about page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import '@/styles/globals.scss'
+// import "@/../../node_modules";
+// Module not found: Can't resolve '../node_modules/font-awesome/css/font-awesome.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
