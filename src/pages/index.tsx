7e8a84862048c3ee2864adc160eba05ee8fde6e2
Chinecherem Harry <chinecherempharry@gmail.com>
2024-01-16 14:46:06 +0100

    worked on the seo for title, keywords and desc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Layout, Metrics, OurValue, Slider, WhyChooseUs, TeachersAndFacilities, 
 
 export default function Home() {
   return (
-    <Layout title={'Dayspring Home | Dayspring Academy official website'} keywords={'Dayspring, Home, EduTech, School, Aba, Abia state, Nigeria'} description={'Welcome to Dayspring Academy'}>
+    <Layout title={'Dayspring Academy Aba: Premier Nursery, Primary, and Secondary School in Abia, Nigeria'} keywords={'Nursery, Primary, Secondary School Aba, Best School in Aba, Nigeria, Education in Abia State, Dayspring Academy Abia, Top-rated School Nigeria, Academic Excellence Aba, Quality Education Nursery Abia, Leading Secondary School Aba, Learning Environment Abia'} description={'Discover excellence in education at Dayspring Academy in Aba, Abia State. We provide quality nursery, primary, and secondary education, fostering academic achievement in a conducive learning environment. Choose Dayspring Academy for top-notch education in Nigeria.'}>
       <>
         <div className="relative">
           <Slider />
